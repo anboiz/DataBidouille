3b4f657cc6cb151e1c0d68d51b43126ed14a5e6b
AnBoiz <antoine.boisyvon@protonmail.com>
2023-04-08 23:39:28 +0200

    debut exercice 1.2

--- exercice_1_utils.py
+++ exercice_1_utils.py
@@ -1,16 +1,17 @@
 import sys
 import os
-cwd = os.getcwd()
-print(cwd)
 
 from src import verification
 import pandas as pd
 
 data = pd.read_excel('data/batiments/batiments_00.xlsx')
+correction_exercice_1_1 = verification.Verification(data)
 
-correction_exercice = verification.Verification(data)
+data = pd.read_csv('data/laposte_hexasmal.csv',sep=";")
+correction_exercice_1_2 = verification.Verification(data)
+
 
 if __name__ == '__main__':
-    data_u = pd.read_excel('data/batiments/batiments_01.xlsx')
+    data_u = pd.read_csv('data/laposte_hexasmal.csv',sep=";")
 
-    correction_exercice.check(data_u)
+    correction_exercice_1_2.check(data_u)
