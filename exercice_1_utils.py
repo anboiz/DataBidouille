b096b8609d0a4dc630d29dc2999140da614209de
AnBoiz <antoine.boisyvon@protonmail.com>
2023-04-13 00:21:55 +0200

    Exercice 1.3 ok

--- exercice_1_utils.py
+++ exercice_1_utils.py
@@ -1,17 +1,22 @@
-import sys
+from pathlib import Path
 import os
 
 from src import verification
 import pandas as pd
 
 data = pd.read_excel('data/batiments/batiments_00.xlsx')
-correction_exercice_1_1 = verification.Verification(data)
+correction_exercice_1_1 = verification.Verification_df(data)
 
 data = pd.read_csv('data/laposte_hexasmal.csv',sep=";")
-correction_exercice_1_2 = verification.Verification(data)
+correction_exercice_1_2 = verification.Verification_df(data)
 
+correction_exercice_1_3_1 = verification.Verification_any(Path('data/batiments'))
+
+file_gen = Path('data/batiments').glob('*.xlsx')
+correction_exercice_1_3_2 = verification.Verification_list(file_gen)
+
+data_liste = [pd.read_excel(e) for e in file_gen]
+correction_exercice_1_3_3 = verification.Verification_df(pd.concat(data_liste))
 
 if __name__ == '__main__':
-    data_u = pd.read_csv('data/laposte_hexasmal.csv',sep=";")
-
-    correction_exercice_1_2.check(data_u)
+    print([x for x in Path('data/batiments').glob('**/*') if x.is_file()])
--- src/verification.py
+++ src/verification.py
@@ -1,8 +1,44 @@
 from colorama import Fore, Style # just_fix_windows_console
 import random
 import pandas as pd
+from typing import List, Any
+from pathlib import Path
 
 class Verification():
+    # def __init__(self,data:pd.DataFrame) -> None:
+    #     self.data = data
+
+    # def check(self, userdata:pd.DataFrame) -> None:
+    #     try:
+    #         verif = self.data.compare(userdata)
+
+    #     except ValueError:
+    #         self.fail()
+    #         return
+
+    #     if verif.size > 0:
+    #         self.fail()
+    #         return
+
+    #     self.success()
+    #     return
+
+    def success(self) -> None:
+        print(Fore.GREEN + '👏 Bravo ! 🥳')
+        print(Style.RESET_ALL)
+    
+    def fail(self) -> None:
+        answers = [
+            "😯 Nope, pas exactement. Essaie encore",
+            "🦕 Bad luck kid, maybe next time. Come back whenever you're ready",
+            "Le grand serpent ne se laisse pas dompter si facilement... 🐉",
+            "Encore un peu d’entraînement ? 🤕"
+            
+        ]
+        print(Fore.RED + f'{random.choice(answers)}')
+        print(Style.RESET_ALL)
+
+class Verification_df(Verification):
     def __init__(self,data:pd.DataFrame) -> None:
         self.data = data
 
@@ -21,33 +57,49 @@ class Verification():
         self.success()
         return
 
-    def success(self) -> None:
-        print(Fore.GREEN + '👏 Bravo ! 🥳')
-        print(Style.RESET_ALL)
-    
-    def fail(self) -> None:
-        answers = [
-            "😯 Nope, pas exactement. Essaie encore",
-            "🦕 Bad luck kid, maybe next time. Come back whenever you're ready",
-            "Le grand serpent ne se laisse pas dompter si facilement... 🐉",
-            "Encore un peu d’entraînement ? 🤕"
+class Verification_list(Verification):
+    def __init__(self,data:List) -> None:
+        self.data = data
+
+    def check(self, userdata:List) -> None:
+        try:
+            result = all(x == y for x, y in zip(self.data, userdata))
             
-        ]
-        print(Fore.RED + f'{random.choice(answers)}')
-        print(Style.RESET_ALL)
+        except ValueError:
+            self.fail()
+            return
+
+        if not result:
+            self.fail()
+            return
+
+        self.success()
+        return
+    
+class Verification_any(Verification):
+    def __init__(self,data:Any) -> None:
+        self.data = data
+
+    def check(self, userdata:Any) -> None:
+        try:
+            result = self.data == userdata
+            
+        except ValueError:
+            self.fail()
+            return
+
+        if not result:
+            self.fail()
+            return
+
+        self.success()
+        return
 
 if __name__ == '__main__':
-    a = pd.DataFrame({"col1":[1,2,3],"col2":[4,5,6]})
-    exo = Verification(a)
-
-    b = a.copy()
-
-    exo.check(b)
-
-    b['col3'] = None
-    exo.check(b)
+    a = Path('data/batiments').glob('.xlsx')
+    exo = Verification_list(a)
+    exo.check(Path('data/batiments').glob('.xlsx'))
+    pass
 
 
-    b.loc[2,"col2"] = "nnbbjsmkd"
 
-    exo.check(b)
